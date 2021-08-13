3b2481fc92e97932d45b22383e01f1c05e94c68f
lhb <1040696765@qq.com>
2021-08-13 14:52:19 +0800

    first commit

---
+++ src/main/java/com/mall/xiaomi/controller/CarouselController.java
@@ -0,0 +1,28 @@
+package com.mall.xiaomi.controller;
+
+import com.mall.xiaomi.pojo.Carousel;
+import com.mall.xiaomi.service.CarouselService;
+import com.mall.xiaomi.util.ResultMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CarouselController {
+
+    @Autowired
+    private ResultMessage resultMessage;
+    @Autowired
+    private CarouselService carouselService;
+
+    //首页轮播图
+    @GetMapping("/resources/carousel")
+    public ResultMessage carousels() {
+        List<Carousel> carousels = carouselService.getCarouselList();
+        resultMessage.success("001", carousels);
+        return resultMessage;
+    }
+
+}
